83f8548df77c2195042604f36cdeae72e70c3b34
Romain Feron <romain.feron@evobio.eu>
2017-06-02 11:22:42 +0200

    quick fix

--- radseq_analyses_pipeline/analyses/analysis.py
+++ radseq_analyses_pipeline/analyses/analysis.py
@@ -14,19 +14,21 @@ def run(global_parameters):
     # catalog_path = [f for f in files if 'catalog.tags' in f][0]
 
     # Path to haplotypes file (batch_X.haplotypes.tsv)
-    haplotype_path = [f for f in files if 'haplotypes' in f][0]
+    haplotype_path = [f for f in files if 'haplotypes' in f]
+    if len(haplotype_path) > 0:
+        print(' - Extracting sex variable loci from haplotypes file ...')
+        loci_data = haplotypes.analyse(haplotype_path[0], global_parameters)
 
     # Paths to all individual tags files (individual.tags.tsv.gz)
     # tags_paths = [f for f in files if 'tags' in f and 'catalog' not in f]
 
-    print(' - Extracting sex variable loci from haplotypes file ...')
-    loci_data = haplotypes.analyse(haplotype_path, global_parameters)
 
-    print(' - Extracting corresponding IDs from catalog file ...')
+
+    # print(' - Extracting corresponding IDs from catalog file ...')
     # loci_to_extract = catalog.analyse(catalog_path, loci_data, global_parameters)
 
-    print(' - Extracting data from individual files...')
+    # print(' - Extracting data from individual files...')
     # individual_tags.analyse(tags_paths, loci_to_extract, loci_data, global_parameters)
 
-    print(' - Testing for neomales...(progress in neomales_data.log)')
+    # print(' - Testing for neomales...(progress in neomales_data.log)')
     # neomales.analyse(haplotype_path, global_parameters)
