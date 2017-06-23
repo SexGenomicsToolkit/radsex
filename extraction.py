19a4da9683434da401f384b694fc9dd234e2e831
Romain Feron <romain.feron@evobio.eu>
2017-06-23 15:32:26 +0200

    bugfix genotoul

--- extraction.py
+++ extraction.py
@@ -18,11 +18,11 @@ if not os.path.isdir(os.path.join(extraction_dir, species)):
 if replicate:
     if not os.path.isdir(os.path.join(extraction_dir, species, replicate)):
         os.mkdir(os.path.join(extraction_dir, species, replicate))
-    files_dir = os.path.join(root_dir, 'results', species, replicate, str(m_value))
-    analyses_dir = os.path.join(extraction_dir, species, replicate, str(m_value))
+    files_dir = os.path.join(root_dir, 'results', species, replicate, 'm_' + str(m_value))
+    analyses_dir = os.path.join(extraction_dir, species, replicate, 'm_' + str(m_value))
 else:
-    files_dir = os.path.join(root_dir, 'results', species, str(m_value))
-    analyses_dir = os.path.join(extraction_dir, species, str(m_value))
+    files_dir = os.path.join(root_dir, 'results', species, 'm_' + str(m_value))
+    analyses_dir = os.path.join(extraction_dir, species, 'm_' + str(m_value))
 
 # Path to population map
 popmap_path = os.path.join(root_dir, 'data', 'popmaps', species + '_popmap.csv')
