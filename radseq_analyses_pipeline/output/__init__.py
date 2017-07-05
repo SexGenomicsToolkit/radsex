7aed432f13df9d25babef6bfd11d36c3c4a99ca1
Romain Feron <romain.feron@evobio.eu>
2017-07-05 14:06:45 +0200

    Inits for new modules

---
+++ radseq_analyses_pipeline/output/__init__.py
@@ -0,0 +1,4 @@
+from loci_matrix import loci_matrix
+from individual_sequences import individual_sequences
+from sex_linked_haplotypes import sex_linked_haplotypes
+from stacks_privacy import stacks_privacy
