543ee845c24e52d425afef7ca197c97e3929b5ad
Romain Feron <romain.feron@evobio.eu>
2017-08-09 09:22:23 +0200

    Improved memory for loading haplotypes

--- radseq_analysis/file_handler/haplotypes.py
+++ radseq_analysis/file_handler/haplotypes.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from radseq_analysis.shared.commons import *
 
 
-def get_haplotypes(haplotypes_file_path, global_parameters):
+def get_haplotypes(haplotypes_file_path, global_parameters, haplotypes=True, numbers=True):
 
     '''
     Extract haplotypes information, sorted by sex, from a haplotypes file
@@ -20,29 +20,32 @@ def get_haplotypes(haplotypes_file_path, global_parameters):
     line = haplotypes_file.readline()
     names = line[:-1].split('\t')[2:]
 
-    haplotypes = dict()
+    if haplotypes:
+        haplotypes_data = dict()
+    if numbers:
+        haplotypes_numbers = dict()
 
     # Sort individual haplotypes by sex for each catalog haplotype
     for line in haplotypes_file:
         tabs = line[:-1].split('\t')
         locus_id = tabs[0]
-        haplotypes[locus_id] = {names[i]: seq for i, seq in enumerate(tabs[2:])}
+        temp = {names[i]: seq for i, seq in enumerate(tabs[2:])}
+        if haplotypes:
+            haplotypes_data[locus_id] = temp
+        if numbers:
+            tags = defaultdict(lambda: {MALES: 0, FEMALES: 0})
+            for individual, haplotype in temp.items():
+                if individual in global_parameters.popmap.keys():
+                    if global_parameters.popmap[individual] is 'M':
+                        tags[haplotype][MALES] += 1
+                    elif global_parameters.popmap[individual] is 'F':
+                        tags[haplotype][FEMALES] += 1
+            haplotypes_numbers[locus_id] = tags
 
-    haplotypes_numbers = dict()
-
-    for locus_id, data in haplotypes.items():
-
-        tags = defaultdict(lambda: {MALES: 0, FEMALES: 0})
-
-        for individual, haplotype in data.items():
-
-            if individual in global_parameters.popmap.keys():
-
-                if global_parameters.popmap[individual] is 'M':
-                    tags[haplotype][MALES] += 1
-                elif global_parameters.popmap[individual] is 'F':
-                    tags[haplotype][FEMALES] += 1
-
-        haplotypes_numbers[locus_id] = tags
-
-    return haplotypes, haplotypes_numbers
+    if haplotypes:
+        if numbers:
+            return haplotypes_data, haplotypes_numbers
+        else:
+            return haplotypes_data
+    elif numbers:
+        return haplotypes_numbers
