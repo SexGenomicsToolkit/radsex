37c7bb9241f80600424ac71a6edffe16496de382
Romain Feron <romain.feron@evobio.eu>
2017-08-30 13:53:05 +0200

    Implemented frequencies visualization

--- radseq_analysis/modules/visualize.py
+++ radseq_analysis/modules/visualize.py
@@ -2,7 +2,7 @@ from radseq_analysis import visualization
 
 
 def detect_plot_type(input_file):
-    return 'clustering'
+    return 'frequencies'
 
 
 def analysis(input_file_path, popmap_file_path, output_file_path, parameters):
@@ -10,6 +10,13 @@ def analysis(input_file_path, popmap_file_path, output_file_path, parameters):
     plot_type = detect_plot_type(input_file_path)
 
     if plot_type == 'haplotypes':
-        visualization.haplotypes(input_file_path, output_file_path, parameters.species)
+        visualization.haplotypes(input_file_path,
+                                 output_file_path,
+                                 parameters.species)
     elif plot_type == 'clustering':
-        visualization.clustering(input_file_path, popmap_file_path, output_file_path)
+        visualization.clustering(input_file_path,
+                                 popmap_file_path,
+                                 output_file_path)
+    elif plot_type == 'frequencies':
+        visualization.frequencies(input_file_path,
+                                  output_file_path)
--- radseq_analysis/visualization/__init__.py
+++ radseq_analysis/visualization/__init__.py
@@ -1,2 +1,3 @@
 from radseq_analysis.visualization.haplotypes import haplotypes
 from radseq_analysis.visualization.clustering import clustering
+from radseq_analysis.visualization.frequencies import frequencies
