1c905c2ca6c56d4feeff124e80d8f140260694e6
RomainFeron <romainferon91@gmail.com>
2020-02-26 12:17:52 +0100

    Added license notice headers

--- src/depth.h
+++ src/depth.h
@@ -1,3 +1,21 @@
+/*
+* Copyright (C) 2020 Romain Feron
+* This file is part of RADSex.
+
+* RADSex is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* RADSex is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with RADSex.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 #include <algorithm>
 #include <fstream>
@@ -5,7 +23,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include "depth_table.h"
+#include "table_parser.h"
 #include "parameters.h"
 #include "popmap.h"
 #include "utils.h"
--- src/table_parser.cpp
+++ src/table_parser.cpp
@@ -1,4 +1,22 @@
-#include "depth_table.h"
+/*
+* Copyright (C) 2020 Romain Feron
+* This file is part of RADSex.
+
+* RADSex is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+
+* RADSex is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with RADSex.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "table_parser.h"
 
 void table_parser(Parameters& parameters, const Popmap& popmap, MarkersQueue& markers_queue, std::mutex& queue_mutex, bool& parsing_ended, bool no_seq, bool no_group) {
 
